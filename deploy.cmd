072bcabd3ab80b14cf52fc1b84e36a81be94e7ad
dream-25 <biswasmohan2529@gmail.com>
2022-03-25 20:55:23 +0530

    @ItsMohan025   03/25/2022  20:55:23.17 PM

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,12 @@
+
+//git init
+//git add README.md
+//git commit -m "Initial"
+//git branch -M main
+//git remote add origin https://github.com/dream-25/q2scomplete.git
+//git push -u origin main
+
+
 cls
 cd F:\Working Ones\DRM-25\Flutter\OCLIF\ocliffadminpanel
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
